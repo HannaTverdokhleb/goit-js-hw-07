5c009bf6063ec601a23e3e9bcdd2ce4a450bebcd
HannaTverdokhleb <1annakiss92@gmail.com>
2021-11-07 21:50:42 +0200

    ht7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const myGallary = { galleryItems };
 
-console.log(galleryItems);
+let galleryContainer = document.querySelector('.gallery');
+
+let galleryInnerContent = galleryItems.map(function (item) {
+  return renderGalleryItems(item);
+});
+
+galleryContainer.innerHTML = galleryInnerContent.join('');
+
+function renderGalleryItems(item) {
+  return `<div class="gallery__item">
+       <a class="gallery__link" href="${item.original}">
+          <img
+             class="gallery__image"
+             src="${item.preview}"
+             data-source="${item.original}"
+             alt="${item.description}"
+          />
+       </a>
+       </div>`;
+}
+
+const links = document.querySelectorAll('.gallery__link');
+
+links.forEach((item) => {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    const url = e.target.getAttribute('data-source');
+    const instance = basicLightbox.create(
+      `<img src="${url}" width="800" height="600">`,
+      {
+        onShow: (instance) => {
+          window.addEventListener('keydown', onEscDown);
+        },
+        onClose: (instance) => {
+          window.removeEventListener('keydown', onEscDown);
+        },
+      }
+    );
+
+    function onEscDown(event) {
+      if (event.key === 'Escape') {
+        instance.close();
+      }
+    }
+
+    instance.show();
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let galleryContainer = document.querySelector('.gallery');
+
+let galleryInnerContent = galleryItems.map(function (item) {
+  return renderGalleryItems(item);
+});
+
+galleryContainer.innerHTML = galleryInnerContent.join('');
+
+function renderGalleryItems(item) {
+  return `<li>
+         <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+         </a>
+      </li>`;
+}
+
+const links = document.querySelectorAll('.gallery__item');
+
+links.forEach((item) => {
+  var lightbox = new SimpleLightbox(item, {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  console.log(item.firstElementChild.getAttribute('alt'));
+});
